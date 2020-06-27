ee86e6c05eca4bc8658650e28fcce55aafe19641
deeja@users.noreply.github.com <deeja@users.noreply.github.com>
2020-06-27 13:25:30 +0100

    Removing margins on the cta2

--- components/CallToAction2Col.vue
+++ components/CallToAction2Col.vue
@@ -1,47 +1,36 @@
 <template>
-  <section 
-           v-editable="blok">
-    <div >
-      <div class="flex justify-center">
-        <div class="w-full  sm:w-3/12 text-center"
-             v-if="blok.style == 'right'">
-          <a class="button-standard"
-             :href="blok.button_link.cached_url">
-            {{ blok.button_text }}
-          </a>
-        </div>
-        <div class="w-full  sm:w-9/12 text-center sm:text-right mt-4 mt-0"
-             v-if="blok.style == 'right'">
-          <h2>{{ blok.headline }}</h2>
-        </div>
-        <div class="w-full  sm:w-9/12 text-left"
-             v-if="blok.style != 'right'">
-          <h2>{{ blok.headline }}</h2>
-        </div>
-        <div class="w-full  sm:w-3/12 text-left sm:text-center mt-4 sm:mt-0"
-             v-if="blok.style != 'right'">
-          <a class="button-standard"
-             :href="blok.button_link.cached_url">
-            {{ blok.button_text }}
-          </a>
-        </div>
+  <section v-editable="blok" class="mb-2">
+    <div class="flex justify-center">
+      <div class="w-full sm:w-3/12 text-center" v-if="blok.style == 'right'">
+        <a class="button-standard" :href="blok.button_link.cached_url">{{ blok.button_text }}</a>
       </div>
+      <div
+        class="w-full sm:w-9/12 text-center sm:text-right"
+        v-if="blok.style == 'right'"
+      >
+        <h2>{{ blok.headline }}</h2>
+      </div>
+      <div class="w-full sm:w-9/12 text-left" v-if="blok.style != 'right'">
+        <h2>{{ blok.headline }}</h2>
+      </div>
+      <div
+        class="w-full sm:w-3/12 text-left sm:text-center"
+        v-if="blok.style != 'right'"
+      >
+        <a class="button-standard" :href="blok.button_link.cached_url">{{ blok.button_text }}</a>
+      </div>
+    </div>
 
-      <div class="row pt-5"
-           v-if="blok.image">
-        <div class="w-full ">
-          <img alt="image"
-               class="img-fluid"
-               :src="blok.image">
-        </div>
+    <div class="row pt-5" v-if="blok.image">
+      <div class="w-full">
+        <img alt="image" class="img-fluid" :src="blok.image" />
       </div>
     </div>
   </section>
-
 </template>
 
 <script>
 export default {
-  props:{blok: {required: true, type: Object}}
-}
+  props: { blok: { required: true, type: Object } }
+};
 </script>
