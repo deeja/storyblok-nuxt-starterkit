c0722ae3b1cfda7124711f89bb864b9f98ea1c52
deeja@users.noreply.github.com <deeja@users.noreply.github.com>
2020-06-28 23:24:30 +0100

    Fixed broken edit mode checker

--- plugins/enable_draft_mode.js
+++ plugins/enable_draft_mode.js
@@ -1,6 +1,9 @@
-export default function({ route: { query }, store }) {
+import {isEditMode} from './../helpers/helper'
+
+export default function({ app, store , route: {query}}) {
   // check if in draft mode using query string storyblok token
-  const draftMode = !!query._storyblok;
+
+  const draftMode = isEditMode(app, query);
   if (store.state.draftMode !== draftMode) {    
     return store.commit("SET_DRAFT_MODE", draftMode);
   }
