490d82af7fffb5d86cb69781320c5f3f5b87c7e4
deeja@users.noreply.github.com <deeja@users.noreply.github.com>
2020-06-28 23:48:18 +0100

    React to edits when mounted, and not by story

--- pages/_.vue
+++ pages/_.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="lg:px-5 md:p-1">
     <div v-if="!story">No story found</div>
-    <component 
+    <component
       v-else-if="story.content.component"
       :key="story.content._uid"
       :blok="story.content"
@@ -10,21 +10,20 @@
   </div>
 </template>
 <script>
-import {
-  reactToEdits
-} from "@/helpers/Storyblok";
-export default {  
+import { reactToEdits } from "@/helpers/Storyblok";
+export default {
   computed: {
     story() {
-      const story = this.$store.getters.getStoryByRoute(this.$route);
-      if (process.client && this.$store.state.draftMode) {
-        reactToEdits(this, story);
-      }
-      return story;
+      return this.$store.getters.getStoryByRoute(this.$route);
     }
   },
-  validate({store, route}) {
+  validate({ store, route }) {
     return store.getters.getStoryByRoute(route);
+  },
+  mounted() {
+    if (process.client && this.$store.state.draftMode) {
+      reactToEdits(this);
+    }
   }
 };
 </script>
