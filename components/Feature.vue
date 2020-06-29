ad3f5e221d493beba99cd63b85b13c2fa8952028
deeja@users.noreply.github.com <deeja@users.noreply.github.com>
2020-06-29 16:44:32 +0100

    Feature list needed some cleaning up

--- components/Feature.vue
+++ components/Feature.vue
@@ -1,12 +1,14 @@
 <template>
-  <div v-editable="blok" class="bg-white w-full sm:w-6/12 lg:w-3/12 pt-4 lg:pt-0">
-    <div class="h-32">
-      <img alt="image" class="max-h-full m-auto" :src="blok.icon" />
+  <div v-editable="blok" class="lg:w-3/12 sm:w-6/12 w-full">
+    <div class="bg-white p-1 rounded-md m-1 h-full">
+      <div class="h-32">
+        <img alt="image" class="max-h-full m-auto" :src="blok.icon" />
+      </div>
+      <h3>
+        <strong>{{ blok.headline }}</strong>
+      </h3>
+      <p>{{ blok.text }}</p>
     </div>
-    <h3>
-      <strong>{{ blok.headline }}</strong>
-    </h3>
-    <p>{{ blok.text }}</p>
   </div>
 </template>
 
