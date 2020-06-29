808f75ab8e542ca84d0310ca20a887874deac2de
deeja@users.noreply.github.com <deeja@users.noreply.github.com>
2020-06-29 01:07:52 +0100

    Enable preview and fetch the current story from the api

--- plugins/enable_draft_mode.js
+++ plugins/enable_draft_mode.js
@@ -1,10 +1,12 @@
 import {isEditMode} from './../helpers/helper'
 
-export default function({ app, store , route: {query}}) {
+export default function({ app, store , route: {query}, enablePreview}) {
   // check if in draft mode using query string storyblok token
-
-  const draftMode = isEditMode(app, query);
-  if (store.state.draftMode !== draftMode) {    
-    return store.commit("SET_DRAFT_MODE", draftMode);
+  
+  if (isEditMode(app, query)) {    
+    // enable preview if exists https://nuxtjs.org/blog/going-full-static/
+    enablePreview && enablePreview(); 
+    store.commit("SET_DRAFT_MODE", true);
+    return store.dispatch("fetchStoryBySlug", query._storyblok)
   }
 }
